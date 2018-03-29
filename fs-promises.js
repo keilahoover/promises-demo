5bfa18bcf015b5212526ae299e8ede51e29acbf0
Keila Hoover <keila.hoover@gmail.com>
2018-03-29 10:56:18 -0600

    Add a file to read

---
+++ fs-promises.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+const fs = require('fs');
+
+function readFile(filename) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      flag: 'r',
+      encoding: 'utf-8',
+    }
+
+    fs.readFile(filename, options, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+readFile('README.dsaflj')
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(chalk.bgRed(err));
+  })
+
+console.log(chalk.green('Promise is now pending'));
